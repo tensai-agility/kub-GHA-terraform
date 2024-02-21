8d9178449fcc69c608f062f102572d5b59e28690
shashidhar-hexa <shashidharh@hexaware.com>
2024-02-21 09:53:14 +0000

    Declare Terraform variables

--- main.tf
+++ main.tf
@@ -1,1 +1,12 @@
 output "message" { value = "Hello, World!" }
+variable "admin_password" {}
+echo "##active_line2##"
+variable "vm_name" {}
+echo "##active_line3##"
+variable "location" {}
+echo "##active_line4##"
+variable "vm_size" {}
+echo "##active_line5##"
+variable "image" {}
+echo "##active_line6##"
+variable "admin_username" {}
